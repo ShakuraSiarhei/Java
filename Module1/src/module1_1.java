970d9d1b185bbe139b5003a64316193c867f8985
ShakuraSiarhei <sergeyshakuro@gmail.com>
2021-10-13 21:26:44 +0300

    done test1

---
+++ Module1/src/module1_1.java
@@ -0,0 +1,10 @@
+public class module1_1 {
+    public static void main (String[] args){
+        double a = 7;
+        double b = 5;
+        double c = 3.22;
+        double z = ((a - 3) * b / 2) + c;
+        System.out.println(z);
+    }
+
+}
